01fe6524984a3a55f0b31e39892d5a14c87f5b07
Narh Henry Padi Dromor <hpdnarh@gmail.com>
2026-05-24 02:50:46 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Next.js 14 has App Router enabled by default
+  serverExternalPackages: ['@prisma/client', 'prisma']
 }
 
 module.exports = nextConfig
