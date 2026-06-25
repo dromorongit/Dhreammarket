aa3f9d1bfd3f5b7ffef87e5c427832b248010a63
Narh Henry Padi Dromor <hpdnarh@gmail.com>
2026-06-25 10:01:08 +0000

    Update page.tsx

--- app/marketplace/product/[id]/page.tsx
+++ app/marketplace/product/[id]/page.tsx
@@ -126,7 +126,7 @@ export default async function ProductPage({ params }: { params: { id: string } }
 
   const productForJsonLd = {
     ...product,
-    images: (product.images ?? []).map((img, i) => ({ ...img, id: img.id ?? String(i) })),
+    images: (product.images ?? []).map((img, i) => ({ ...img, id: String(i) })),
   }
 
   return (
