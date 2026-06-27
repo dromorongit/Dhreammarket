2d236fe9d29ce0a1c738eba88c3e983a2b8612f6
Narh Henry Padi Dromor <hpdnarh@gmail.com>
2026-06-27 01:47:18 +0000

    Prisma fix

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,7 +3,5 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  datasource: {
-    url: process.env.DATABASE_URL,
-  },
+  datasourceUrl: process.env.DATABASE_URL,
 });
