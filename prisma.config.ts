ff14bb7dfbbca54867a00bd6dbdcae70571a59d7
Narh Henry Padi Dromor <hpdnarh@gmail.com>
2026-06-27 01:05:45 +0000

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,14 +1,6 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
-import { PrismaPg } from "@prisma/adapter-pg";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrate: {
-    adapter: () => {
-      const connectionString = process.env.DATABASE_URL;
-      if (!connectionString) throw new Error("DATABASE_URL is not set");
-      return new PrismaPg({ connectionString });
-    },
-  },
 });
