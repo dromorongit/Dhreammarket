02cfa7e9ca41557e5d2434562cdc1ec6fb4af171
Narh Henry Padi Dromor <hpdnarh@gmail.com>
2026-07-21 14:03:18 +0000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Head>
-      <script
-        src="https://www.googletagmanager.com/gtag/js?id=G-QCL4XW5FR1"
-        async
-      />
-      <script
-        nonce="{Math.random().toString(36).substr(2, 9)}"
-        dangerouslySetInnerHTML={{ __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-QCL4XW5FR1');` }}
-      />
-    </Head>
-    <Component {...pageProps} />
+    <>
+      <Head>
+        <script
+          src="https://www.googletagmanager.com/gtag/js?id=G-QCL4XW5FR1"
+          strategy="lazyOnload"
+        />
+        <Script
+          strategy="beforeInteractive"
+        >
+          {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-QCL4XW5FR1');`}
+        </Script>
+      </Head>
+      <Component {...pageProps} />
+    </>
   );
 }
 
