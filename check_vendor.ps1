e74eaba0dd73220d4dfc97a3b728b3078899d48c
Narh Henry Padi Dromor <hpdnarh@gmail.com>
2026-07-31 12:05:32 +0000

    New commits

--- check_vendor.ps1
+++
@@ -1,9 +0,0 @@
-$c = Get-Content -LiteralPath '.\app\vendor\[id]\vendor-client.tsx'
-$open = ($c | Select-String -Pattern '\(' -SimpleMatch).Count
-$close = ($c | Select-String -Pattern '\)' -SimpleMatch).Count
-'Open parens: ' + $open
-'Close parens: ' + $close
-$openDiv = ($c | Select-String -Pattern '<div' -SimpleMatch).Count
-$closeDiv = ($c | Select-String -Pattern '</div>' -SimpleMatch).Count
-'Open divs: ' + $openDiv
-'Close divs: ' + $closeDiv
