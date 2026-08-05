c6f9c8629b4999a66248e3166d5b7a0991f80dd6
Narh Henry Padi Dromor <hpdnarh@gmail.com>
2026-08-05 11:43:33 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { Navbar } from '@/components/Navbar'
-import { TopContactBar } from '@/components/TopContactBar'
+import TopContactBar from '@/components/TopContactBar'
 import { Footer } from '@/components/Footer'
 import { CartProvider } from '@/lib/CartContext'
 import QueryProvider from '@/components/QueryProvider'
