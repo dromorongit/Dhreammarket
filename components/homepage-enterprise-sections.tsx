943d72cb6a83a6a768e444bf9402974ba718412b
Narh Henry Padi Dromor <hpdnarh@gmail.com>
2026-08-17 15:49:40 +0000

    DHREAM MARKET v1.5

--- components/homepage-enterprise-sections.tsx
+++ components/homepage-enterprise-sections.tsx
@@ -225,16 +225,16 @@ function EnterpriseSectionSkeleton({ dark = false }: { dark?: boolean }) {
           />
         </div>
         <div className='space-y-4'>
-          <div className='overflow-x-auto overflow-y-hidden scrollbar-hide scroll-smooth snap-x snap-mandatory -mx-4 px-4 sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8 pb-4'>
-            <div className='flex gap-4'>
-              {[...Array(5)].map((_, i) => (
-                <div key={i} className="snap-start flex-shrink-0 w-[calc(50%-8px)] sm:w-[calc(25%-12px)] lg:w-[calc(20%-12px)]">
-                  <SkeletonCard />
-                </div>
-              ))}
-            </div>
-          </div>
-          <div className='overflow-x-auto overflow-y-hidden scrollbar-hide scroll-smooth snap-x snap-mandatory -mx-4 px-4 sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8 pb-4'>
+          <div className='overflow-x-auto overflow-y-hidden scrollbar-hide scroll-smooth snap-x snap-mandatory -mx-4 px-4 sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8'>
+             <div className='flex gap-4'>
+               {[...Array(5)].map((_, i) => (
+                 <div key={i} className="snap-start flex-shrink-0 w-[calc(50%-8px)] sm:w-[calc(25%-12px)] lg:w-[calc(20%-12px)]">
+                   <SkeletonCard />
+                 </div>
+               ))}
+             </div>
+           </div>
+           <div className='overflow-x-auto overflow-y-hidden scrollbar-hide scroll-smooth snap-x snap-mandatory -mx-4 px-4 sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8'>
             <div className='flex gap-4'>
               {[...Array(5)].map((_, i) => (
                 <div key={i} className="snap-start flex-shrink-0 w-[calc(50%-8px)] sm:w-[calc(25%-12px)] lg:w-[calc(20%-12px)]">
--- components/homepage-sections.tsx
+++ components/homepage-sections.tsx
@@ -42,7 +42,7 @@ export function SectionPill({ label, icon, gradientFrom, gradientVia, gradientTo
            <Skeleton className="h-4 w-64" />
          </div>
          <div className="space-y-4">
-           <div className="overflow-x-auto overflow-y-hidden scrollbar-hide scroll-smooth snap-x snap-mandatory -mx-4 px-4 sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8 pb-4">
+            <div className="overflow-x-auto overflow-y-hidden scrollbar-hide scroll-smooth snap-x snap-mandatory -mx-4 px-4 sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
              <div className="flex gap-4">
                {[...Array(5)].map((_, i) => (
 <div key={i} className="snap-start flex-shrink-0 w-[calc(50%-8px)] sm:w-[calc(25%-12px)] lg:w-[calc(20%-12px)]">
